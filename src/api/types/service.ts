5d3d85fd8137a56819d15de62d54d8812218489d
Sparky Kitty <60945689+Sparky-Kitty@users.noreply.github.com>
2023-08-25 11:15:58 -0700

    -Added service type/utils. (#1)
    * -Added service type/utils.
    
    * Added missing columns to interface.
    
    * Tidied up service shared.
    
    * * Cleaned up service type
    * Removed uneeded payload util
    
    * Updated service.ts type to include TAGS.

---
+++ src/api/types/service.ts
@@ -0,0 +1,48 @@
+import { Game } from '@diablosnaps/common';
+import { AuthUser } from './'
+
+export const TAGS = {
+    POWERLEVELING: 1 << 0,
+    BOSS_HELP: 1 << 1,
+    UBER_LILITH: 1 << 2,
+    CAPSTONE_BOOST: 1 << 3,
+    EUROPE: 1 << 4,
+    ASIA: 1 << 5,
+    AMERICA: 1 << 6,
+};
+
+export const numberToTags = (numberValue: number): string[] => {
+    const selectedTags: string[] = [];
+
+    for (const tag in TAGS) {
+        if ((numberValue & TAGS[tag]) !== 0) {
+            selectedTags.push(tag.replace("_", " "));
+        }
+    }
+    return selectedTags;
+}
+
+export interface ServiceListing {
+    id: string;
+    realmType?: string;
+    title?: string;
+    content?: string;
+    user?: AuthUser;
+    userId?: number;
+    tags?: number;
+    maxAcceptedSlots?: number;
+    updatedAt?: Date;
+    deleted?: boolean;
+}
+
+export interface ServiceGetSearchQuery {
+    serverType: Game.ServerType;
+    title?: string;
+    tags?: number;
+    userId?: number;
+    deleted?: boolean;
+    offset?: number;
+    limit?: number;
+}
+
+export type ServiceGetSearchResponse = ServiceListing[]
