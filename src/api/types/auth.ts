aa51d831b1ea56c4ccf2042ee9c6202e70022900
Sparky Kitty <60945689+Sparky-Kitty@users.noreply.github.com>
2023-09-17 18:28:34 -0700

    Feature/notification interface (#5)
    * * Refactored numbers to strings.
    * Added notification interface to Auth type.
    
    * Added vouch data to API.AuthUser
    
    * removed unused name param

--- src/api/types/auth.ts
+++ src/api/types/auth.ts
@@ -1,8 +1,18 @@
+import { API } from "..";
+
 export interface AuthUser {
-    id: string;
+    id?: string;
     battleNetTag: string;
-    discordId: string;
+    discordId?: string;
     discordName: string;
+    vouchRating: number;
+    vouchScore: number;
+}
+
+export interface Notification {
+    recipient: AuthUser;
+    entity: API.ServiceSlot;
+    message: string;
 }
 
 export interface AuthCallbackParams {
